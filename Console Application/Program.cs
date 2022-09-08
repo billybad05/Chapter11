8d9dedf88561abb5454891499f33fd4a11fe0737
Billy Dotson <billybad5@gmail.com>
2022-09-08 15:08:19 -0400

    Add project files.

---
+++ Console Application/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+string[] names = new[] {
+    "Michael", "Pam", "Jimmy", "Dwight", "Angela", "Kevin", "Toby", "Creed"
+};
+
+WriteLine("Deferred execution");
+
+var query1 = names.Where(name => name.EndsWith("m"));
+
+string[] result = query1.ToArray();
+foreach (string name in query1) {
+    WriteLine(name);
+}
